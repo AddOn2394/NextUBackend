fcdfeb28ffba9dc7e40b4be37d2eaa02c915bf6e
AddOn2394 <jose_23marzo@outlook.com>
2018-05-04 08:13:32 -0600

    Finalización de Rutas

--- server/index.js
+++ server/index.js
@@ -1,18 +1,120 @@
-var bodyParser  = require('body-parser'),
-    http        = require('http'),
-    express     = require('express')
+var express = require("express");
+var app = express();
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var path = require('path');
+var _ = require("underscore");
 
-    houses = require('./properties')
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true })); 
 
-var port        = port = process.env.PORT || 3000,
-    app         = express(),
-    Server      = http.createServer(app)
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
-app.use('/houses', houses)
-app.use(express.static('public'))
 
-Server.listen(port, function () {
-    console.log("Server is running on Port: "+ port)
-})
+//Ejemplo: GET http://localhost:3000/items
+app.get('/houses/:city', function (req, res, next) {
+    var dataPath = __dirname + path.join('/data.json')
+
+    return new Promise(function (resolve, reject) {
+        fs.readFile(dataPath, 'utf8', function (err, readData) {
+            if (err) reject(err)
+            resolve(JSON.parse(readData))
+            datos = JSON.parse(readData)
+            var filtered = _.where(datos, {"Ciudad": req.params.city});
+            
+            res.send( filtered)
+        })
+    })
+});
+
+//Ejemplo: GET http://localhost:3000/items
+app.get('/houses', function (req, res, next) {
+    var dataPath = __dirname + path.join('/data.json')
+
+    return new Promise(function (resolve, reject) {
+        fs.readFile(dataPath, 'utf8', function (err, readData) {
+            if (err) reject(err)
+            resolve(JSON.parse(readData))
+            datos = JSON.parse(readData)
+            res.send(datos)
+        })
+    })
+});
+
+//Ejemplo: GET http://localhost:3000/items
+app.get('/cities', function (req, res, next) {
+    var dataPath = __dirname + path.join('/data.json')
+
+    return new Promise(function (resolve, reject) {
+        fs.readFile(dataPath, 'utf8', function (err, readData) {
+            if (err) reject(err)
+            resolve(JSON.parse(readData))
+            datos = JSON.parse(readData)
+            datosSinDuplicados = eliminarObjetosDuplicados(datos, 'Ciudad')
+            res.send(datosSinDuplicados.map(getCities))
+        })
+    })
+});
+
+//Ejemplo: GET http://localhost:3000/items
+app.get('/types', function (req, res, next) {
+    var dataPath = __dirname + path.join('/data.json')
+
+    return new Promise(function (resolve, reject) {
+        fs.readFile(dataPath, 'utf8', function (err, readData) {
+            if (err) reject(err)
+            resolve(JSON.parse(readData))
+            datos = JSON.parse(readData)
+            datosSinDuplicados = eliminarObjetosDuplicados(datos,'Tipo')
+            res.send(datosSinDuplicados.map(getTypes))
+        })
+    })
+});
+
+//Ejemplo: GET http://localhost:3000/items
+app.get('/prices', function (req, res, next) {
+    var dataPath = __dirname + path.join('/data.json')
+
+    return new Promise(function (resolve, reject) {
+        fs.readFile(dataPath, 'utf8', function (err, readData) {
+            if (err) reject(err)
+            resolve(JSON.parse(readData))
+            datos = JSON.parse(readData)
+            datosSinDuplicados = eliminarObjetosDuplicados(datos, 'Precio')
+            res.send(datosSinDuplicados.map(getPrices))
+        })
+    })
+});
+
+function getCities(item, index) {
+    var city = item.Ciudad;
+    return city;
+}
+
+function getTypes(item, index) {
+    var type = item.Tipo;
+    return type;
+}
+
+function getPrices(item, index) {
+    var prices = item.Precio;
+    return prices;
+}
+
+function eliminarObjetosDuplicados(arr, prop) {
+    var nuevoArray = [];
+    var lookup = {};
+
+    for (var i in arr) {
+        lookup[arr[i][prop]] = arr[i];
+    }
+
+    for (i in lookup) {
+        nuevoArray.push(lookup[i]);
+    }
+
+    return nuevoArray;
+}
+
+var server = app.listen(3000, function () {
+    console.log('Server is running..');
+});
